0e1c5d77c784ba3c845a40c2fa68f506ed687ebf
Justin Moriarty <justin-moriarty@live.com>
2021-05-25 19:47:10 +1000

    Add comments to code to make more readable

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+//Create Random numbers
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber1);
-console.log(randomNumber2);
 
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+//Get replacement variables for the dice images and insert into the HTML
+var randomDiceImg = "/images/dice" + randomNumber + ".png"; // /images/dice1-6.png"
+
+//Change the img src to change the dice img to the correct one
+document.querySelector(".img1").setAttribute("src", randomDiceImg);
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
+//Game logic writes result using inner.HTML
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
